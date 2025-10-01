2a59d5ea39b5dbac5f2b68e5538f81d874ee4ce8
djjy112-pi <djjy112@goedu.kr>
2025-10-01 09:17:32 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.write(
 )
 st.success( "반갑습니다. 저는 1-9 이지윤입니다.")
 st.info("반갑습니다. 저는 1-9 이지윤입니다.")
-st.image(https://www.google.com/imgres?q=%ED%95%9C%ED%83%9C%EC%82%B0&imgurl=https%3A%2F%2Fmedia.bunjang.co.kr%2Fproduct%2F348232582_1_1754007424_w360.jpg&imgrefurl=https%3A%2F%2Fm.bunjang.co.kr%2Fproducts%2F348232582&docid=zrHq0DEAHdMJsM&tbnid=MnEc0PncNj_gQM&vet=12ahUKEwiy_9GzyoKQAxXgdfUHHVBBGxUQM3oECCAQAA..i&w=360&h=360&hcb=2&ved=2ahUKEwiy_9GzyoKQAxXgdfUHHVBBGxUQM3oECCAQAA)
+st.image("https://www.google.com/imgres?q=%ED%95%9C%ED%83%9C%EC%82%B0&imgurl=https%3A%2F%2Fmedia.bunjang.co.kr%2Fproduct%2F348232582_1_1754007424_w360.jpg&imgrefurl=https%3A%2F%2Fm.bunjang.co.kr%2Fproducts%2F348232582&docid=zrHq0DEAHdMJsM&tbnid=MnEc0PncNj_gQM&vet=12ahUKEwiy_9GzyoKQAxXgdfUHHVBBGxUQM3oECCAQAA..i&w=360&h=360&hcb=2&ved=2ahUKEwiy_9GzyoKQAxXgdfUHHVBBGxUQM3oECCAQAA")
