3ea70b88e4a80908e64912bbf2cbda5a2c3fa376
djjy112-pi <djjy112@goedu.kr>
2025-10-01 10:57:14 +0000

    아무거나

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "반갑습니다. 저는 1-9 이지윤입니다."
-)
-st.success( "반갑습니다. 저는 1-9 이지윤입니다.")
-st.info("반갑습니다. 저는 1-9 이지윤입니다.")
-st.image("https://www.google.com/imgres?q=%ED%95%9C%ED%83%9C%EC%82%B0&imgurl=https%3A%2F%2Fmedia.bunjang.co.kr%2Fproduct%2F348232582_1_1754007424_w360.jpg&imgrefurl=https%3A%2F%2Fm.bunjang.co.kr%2Fproducts%2F348232582&docid=zrHq0DEAHdMJsM&tbnid=MnEc0PncNj_gQM&vet=12ahUKEwiy_9GzyoKQAxXgdfUHHVBBGxUQM3oECCAQAA..i&w=360&h=360&hcb=2&ved=2ahUKEwiy_9GzyoKQAxXgdfUHHVBBGxUQM3oECCAQAA")
